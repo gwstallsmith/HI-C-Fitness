a702ffe31e979a1fe26839dd5fa0d0fba3c00ff4
Garrett <gwstallsmith@gmail.com>
2023-04-17 13:18:56 -0400

    Testing workout object.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 class Workout {
-    constructor(type, description, ytLink, image) {
-        this.type = type;
-        this.description = description;
-        this.ytLink = ytLink;
-        this.image = image;
+    constructor(name, mg, description, ytLink, image) {
+        this.name_ = name;
+        this.muscleGroup_ = mg;
+        this.description_ = description;
+        this.ytLink_ = ytLink;
+        this.image_ = image;
     }
+
+
+    printWorkout() {
+        console.log("Name: " + this.name_);
+        console.log("Muscle Group: " + this.muscleGroup_);
+        console.log("Description: " + this.description_);
+        console.log("YouTube Link: " + this.ytLink_);
+        console.log("Image (Link): " + this.image_);
+    }
+}
+
+
+function main() {
+    let dbCurls = new Workout("Dumbell Curls", "Upper Body", "blah blah", "https://www.youtube.com/watch?v=ykJmrZ5v0Oo&ab_channel=Howcast", "\\assets\\dumbellCurl.jpg")
+    dbCurls.printWorkout();
 }
