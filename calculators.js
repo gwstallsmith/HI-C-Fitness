6c4b97216fdfe4cfa6e27169066042001fb14ff0
Garrett <gwstallsmith@gmail.com>
2023-04-22 13:13:50 -0400

    BMI calculator doesn't display NaN anymore.

--- calculators.js
+++
@@ -1,8 +0,0 @@
-function getChecked() {
-    const checkBox = document.getElementById('check1').checked;
-    if (checkBox === true) {
-      console.log(true);
-      } else {
-        console.log(false);
-    }
-  }
