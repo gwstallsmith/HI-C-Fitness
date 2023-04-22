49dbccc52043aa0290c423f9dcc857faf3c99834
Garrett <gwstallsmith@gmail.com>
2023-04-21 22:31:31 -0400

    Added BMI calculator.

---
+++ calculators.js
@@ -0,0 +1,8 @@
+function getChecked() {
+    const checkBox = document.getElementById('check1').checked;
+    if (checkBox === true) {
+      console.log(true);
+      } else {
+        console.log(false);
+    }
+  }
